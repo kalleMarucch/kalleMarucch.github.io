d958a3943a74120fd748fcdc675343e6ca75ef66
kalleMarucch <kalle.marucch@gmail.com>
2019-03-30 17:40:41 +0900

    add test page

---
+++ test/EorzeaClock.js
@@ -0,0 +1,31 @@
+var dt = new Date;
+
+var EorzeaClock = (function(){
+    var _name = 'kazushi',
+        _age = 36;
+    
+    function _init() {
+      // 何らかの処理
+    }
+    
+    function _getName() {
+      return _name;
+    }
+    
+    function _getAge() {
+      return _age;
+    }
+    
+    function _doSomething() {
+      //・・・
+    }
+    
+    // 初期化を実行する
+    _init();
+    
+    // 公開APIを返す
+    return {
+      getName: _getName,
+      getAge : _getAge
+    };
+  }());
