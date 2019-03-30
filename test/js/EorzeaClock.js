a455dc17a1bfe8b30722f08bb2c005ba6718b5db
kalleMarucch <kalle.marucch@gmail.com>
2019-03-30 18:22:20 +0900

    fix js output method

--- test/js/EorzeaClock.js
+++ test/js/EorzeaClock.js
@@ -30,12 +30,14 @@ var EorzeaClock = (function(){
     };
   }());
 
-  function showClock1() {
-    var nowTime = new Date();
-    var nowHour = nowTime.getHours();
-    var nowMin  = nowTime.getMinutes();
-    var nowSec  = nowTime.getSeconds();
-    var msg = "現在時刻は、" + nowHour + ":" + nowMin + ":" + nowSec + " です。";
-    document.getElementById("RealtimeClockArea").innerHTML = msg;
+function showClock1() {
+  var nowTime = new Date();
+  var nowHour = nowTime.getHours();
+  var nowMin  = nowTime.getMinutes();
+  var nowSec  = nowTime.getSeconds();
+  var msg = "現在時刻は、" + nowHour + ":" + nowMin + ":" + nowSec + " です。";
+  document.getElementById("RealtimeClockArea").innerHTML = msg; 
  }
+
+ document.getElementById("RealtimeEorzeaClockArea").innerHTML = EorzeaClock.getName;
  setInterval('showClock1()',1000);
