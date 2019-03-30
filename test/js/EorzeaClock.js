cf3c96395b91e12ad38a1729cc317967b3561472
kalleMarucch <kalle.marucch@gmail.com>
2019-03-30 18:47:31 +0900

    fix indent

--- test/js/EorzeaClock.js
+++ test/js/EorzeaClock.js
@@ -1,23 +1,22 @@
-var dt = new Date;
-
 var EorzeaClock = (function(){
     var _name = 'kazushi',
-        _age = 36;
+    _age = 36;
     
     function _init() {
-      // 何らかの処理
+        // 何らかの処理
+        var nowDate = new Date;
     }
     
     function _getName() {
-      return _name;
+        return _name;
     }
     
     function _getAge() {
-      return _age;
+        return _age;
     }
     
     function _doSomething() {
-      //・・・
+        //・・・
     }
     
     // 初期化を実行する
@@ -25,19 +24,25 @@ var EorzeaClock = (function(){
     
     // 公開APIを返す
     return {
-      getName: _getName,
-      getAge : _getAge
+        getName: _getName,
+        getAge : _getAge
     };
-  }());
+}());
+
+
 
 function showClock1() {
-  var nowTime = new Date();
-  var nowHour = nowTime.getHours();
-  var nowMin  = nowTime.getMinutes();
-  var nowSec  = nowTime.getSeconds();
-  var msg = "現在時刻は、" + nowHour + ":" + nowMin + ":" + nowSec + " です。";
-  document.getElementById("RealtimeClockArea").innerHTML = msg; 
- }
+    var nowTime = new Date();
+    var nowHour = nowTime.getHours();
+    var nowMin  = nowTime.getMinutes();
+    var nowSec  = nowTime.getSeconds();
+    var msg = "現在時刻は、" + nowHour + ":" + nowMin + ":" + nowSec + " です。";
+    document.getElementById("RealtimeClockArea").innerHTML = msg; 
+}
 
- document.getElementById("EorzeaClockArea").innerHTML = EorzeaClock.getName();
- setInterval('showClock1()',1000);
+showClock1();
+
+setTimeout(function () {
+    showClock1();  // 最初のジャスト1秒表示
+    setInterval(showClock2, 1000); // 以降のジャスト1秒表示
+}, 1000 - (new Date()).getUTCMilliseconds());
