d34dff9a83aa0ff20dfce37ee940ffbc8490a705
kalleMarucch <kalle.marucch@gmail.com>
2019-04-19 21:32:15 +0900

    fix space

--- ysdaaaaaaaaaaaaaa/js/script.js
+++ ysdaaaaaaaaaaaaaa/js/script.js
@@ -1,7 +1,7 @@
 function ysdaaaaaaaaaaaaaa(){
     var ysda1 = "よしだああああああああああああああ！";
     var ff14Tag = "#FF14 #FFXIV";
-    var UserString = document.getElementById('formControlTextarea1').value;
+    var UserString = document.getElementById('inputTextarea1').value;
     var ysdaaaaaaaaaaaaaa = UserString + " " + ysda1 + " " + ff14Tag;
 
     document.getElementById('outputTextarea1').value = ysdaaaaaaaaaaaaaa;
@@ -20,7 +20,7 @@ document.getElementById('btn-ysda1').onclick = function() {
     ysdaaaaaaaaaaaaaa();
 };
 
-document.getElementById('formControlTextarea1').onkeyup = function() {
+document.getElementById('inputTextarea1').onkeyup = function() {
     var n = Math.random()
     if(n < 0.1){
         limitBreakCharged();
