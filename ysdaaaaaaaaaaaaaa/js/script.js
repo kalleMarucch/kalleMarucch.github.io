d42f90b1cbe1721fa6c3b3b5da90f5a7f8a0a61e
kalleMarucch <kalle.marucch@gmail.com>
2019-04-19 20:59:42 +0900

    add sound

--- ysdaaaaaaaaaaaaaa/js/script.js
+++ ysdaaaaaaaaaaaaaa/js/script.js
@@ -10,3 +10,19 @@ function ysdaaaaaaaaaaaaaa(){
 document.getElementById("btn-ysda1").onclick = function() {
     ysdaaaaaaaaaaaaaa();
 };
+
+(function (window, $) {
+    'use strict';
+
+    $.fn.useSound = function (_event, _id) {
+      var se = $(_id);
+      this.on(_event, function(){
+        se[0].currentTime = 0;
+        se[0].play();
+      });
+      return this;
+    };
+
+  })(this, this.jQuery);
+
+  $('.btn input#btn-ysda1').useSound('mousedown touchstart', '#sound');
