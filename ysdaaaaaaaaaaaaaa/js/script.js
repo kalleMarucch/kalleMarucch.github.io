7fe9cddbaccd91ed9349f5b94b3f374817c4e2ce
kalleMarucch <kalle.marucch@gmail.com>
2019-04-19 20:06:45 +0900

    add js

--- ysdaaaaaaaaaaaaaa/js/script.js
+++ ysdaaaaaaaaaaaaaa/js/script.js
@@ -1,3 +1,9 @@
 function ysdaaaaaaaaaaaaaa(){
-    return null;
+    var ysdaaaaaaaaaaaaaa = "よしだああああああああああああああ！";
+    var UserString = document.getElementById('formControlTextarea1').value;
+    document.getElementById('outputTextarea1').value = UserString + ysdaaaaaaaaaaaaaa;
 };
+
+document.getElementById("btn-ysda1").onclick = function() {
+    ysdaaaaaaaaaaaaaa();
+  };
