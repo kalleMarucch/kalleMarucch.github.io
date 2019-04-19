fd4574e79e0bb725f5c56cb225ede7df1ac853ac
kalleMarucch <kalle.marucch@gmail.com>
2019-04-19 20:31:08 +0900

    fix js

--- ysdaaaaaaaaaaaaaa/js/script.js
+++ ysdaaaaaaaaaaaaaa/js/script.js
@@ -2,7 +2,7 @@ function ysdaaaaaaaaaaaaaa(){
     var ysda1 = "よしだああああああああああああああ！";
     var ff14Tag = "#FF14 #FFXIV";
     var UserString = document.getElementById('formControlTextarea1').value;
-    var ysdaaaaaaaaaaaaaa = UserString + " " + ysdaaaaaaaaaaaaaa + " " + ff14Tag;
+    var ysdaaaaaaaaaaaaaa = UserString + " " + ysda1 + " " + ff14Tag;
 
     document.getElementById('outputTextarea1').value = ysdaaaaaaaaaaaaaa;
 };
