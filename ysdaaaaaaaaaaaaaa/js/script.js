52e919eafb4331801b794e1131f3262120458f1a
kalleMarucch <kalle.marucch@gmail.com>
2019-04-20 23:36:40 +0900

    url test

--- ysdaaaaaaaaaaaaaa/js/script.js
+++ ysdaaaaaaaaaaaaaa/js/script.js
@@ -6,7 +6,7 @@ function ysdaaaaaaaaaaaaaa(){
 
     document.getElementById('outputTextarea1').value = ysdaaaaaaaaaaaaaa;
 
-    var button = '<a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-size="large" data-lang="ja" data-show-count="false" data-url=" " data-text="' + ysdaaaaaaaaaaaaaa + '">Tweet</a>';
+    var button = '<a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-size="large" data-lang="ja" data-show-count="false" data-url="#YSDA" data-text="' + ysdaaaaaaaaaaaaaa + '">Tweet</a>';
     $('#twbtn').html(button);
     twttr.widgets.load();
     //document.getElementById('tweetButton').setAttribute('data-text', ysdaaaaaaaaaaaaaa);
