355e460086495ee091558b1ad32a043bf56a1eac
kalleMarucch <kalle.marucch@gmail.com>
2019-04-21 00:07:02 +0900

    fix text

--- ysdaaaaaaaaaaaaaa/js/script.js
+++ ysdaaaaaaaaaaaaaa/js/script.js
@@ -4,12 +4,10 @@ function ysdaaaaaaaaaaaaaa(){
     var UserString = document.getElementById('inputTextarea1').value;
     var ysdaaaaaaaaaaaaaa = UserString + " " + ysda1 + " " + ff14Tag;
 
-    document.getElementById('outputTextarea1').value = ysdaaaaaaaaaaaaaa;
-
     var button = '<a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-size="large" data-lang="ja" data-show-count="false" data-url="#" data-text="' + ysdaaaaaaaaaaaaaa + '">Tweet</a>';
     $('#twbtn').html(button);
     twttr.widgets.load();
-    //document.getElementById('tweetButton').setAttribute('data-text', ysdaaaaaaaaaaaaaa);
+    //document.getElementById('outputTextarea1').value = ysdaaaaaaaaaaaaaa;
 };
 
 function limitBreakCharged(){
