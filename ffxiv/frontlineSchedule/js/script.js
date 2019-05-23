56396ef9ac5d007830a19892ccbf373d536774a6
kalleMarucch <kalle.marucch@gmail.com>
2019-05-24 00:04:52 +0900

    add frontline schedule

---
+++ ffxiv/frontlineSchedule/js/script.js
@@ -0,0 +1,43 @@
+$(function () {
+    var day1 = new Date('2010/9/30 00:00:00');
+    var day2 = new Date();
+    var dayDiff = Math.floor((day2.getTime() - day1.getTime()) / (1000 * 60 * 60 * 24));
+
+    for (let index = 0; index < 7; index++) {
+
+        var day3 = new Date(day2.getTime());
+        day3.setDate(day3.getDate() + index);
+        var day3Week = [ "日", "月", "火", "水", "木", "金", "土" ][day3.getDay()];
+
+        var day4 = day3.getFullYear() + "/" + (day3.getMonth() + 1) + "/" + day3.getDate() + '(' + day3Week + ')';
+
+        var contentArray = isFrontlineContent((dayDiff + index) % 3);
+
+        $('#frontlineContentDay' + index).text(day4);
+        $('#frontlineContent' + index + '_1').text(contentArray[0]);
+        $('#frontlineContent' + index + '_2').text(contentArray[1]);
+    }
+});
+
+function isFrontlineContent(val) {
+    var array = [];
+
+    switch (val) {
+        case 0:
+            array.push("フィールド・オブ・グローリー");
+            array.push("（砕氷戦）");
+            break;
+        case 1:
+            array.push("外縁遺跡群");
+            array.push("（制圧戦）");
+            break;
+        case 2:
+            array.push("シールロック");
+            array.push("（争奪戦）");
+            break;
+        default:
+            array.push('none');
+            array.push("none");
+    }
+    return array;
+}
