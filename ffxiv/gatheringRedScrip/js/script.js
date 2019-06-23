ee6d753a4df6669596f790dc1cbb115dc9b26d78
kalleMarucch <kalle.marucch@gmail.com>
2019-06-23 19:48:11 +0900

    fix table layout

--- ffxiv/gatheringRedScrip/js/script.js
+++ ffxiv/gatheringRedScrip/js/script.js
@@ -3,14 +3,14 @@ $(function() {
 
         var rows = "";
         for(i = 0; i < json.length; i++){
-            rows += "<tr>";
+            rows += '<tr>';
+            rows += '<td><input type="checkbox" checked></td>';
             for (j = 0; j < json[i].length; j++) {
-                alert(json[i][j]);
-                rows += "<td>";
+                rows += '<td>';
                 rows += json[i][j];
-                rows += "</td>"
+                rows += '</td>';
             }
-            rows += "</td>";
+            rows += '</td>';
         }
 
         $('#miner3 tbody').append(rows);
